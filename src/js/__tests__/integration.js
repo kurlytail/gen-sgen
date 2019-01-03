0a9999882957d6a91bbb688529999b0d16957dbb
ashwin <ashwinkn@hotmail.com>
2019-01-03 15:15:35 -0600

    Created sgen generator

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -1,7 +1,37 @@
+import { execSync } from 'child_process';
 
 describe('# integration test', () => {
-    it('## dummy test', () => {
-        const test = true;
-        expect(test).toBeTruthy();
+    beforeEach(() => {
+        execSync('rm -rf testoutput');
+    });
+
+    it('## should print help options', () => {
+        const output = execSync('./scripts/sgen-sgen.sh -h').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design', () => {
+        const output = execSync('./scripts/sgen-sgen.sh -d src/test/fixture/design.js -o testoutput').toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design with merge', () => {
+        let output = execSync(
+            './scripts/sgen-sgen.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        ).toString();
+        expect(output).toMatchSnapshot();
+        output = execSync(
+            './scripts/sgen-sgen.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        ).toString();
+        expect(output).toMatchSnapshot();
+    });
+
+    it('## should generate design and run npm commands', () => {
+        let output = execSync(
+            './scripts/sgen-sgen.sh -d src/test/fixture/design.js -o testoutput --overwrite=merge'
+        ).toString();
+        expect(output).toMatchSnapshot();
+        output = execSync('npm install', { cwd: 'testoutput' }).toString();
+        output = execSync('npm run lint', { cwd: 'testoutput' }).toString();
     });
 });
