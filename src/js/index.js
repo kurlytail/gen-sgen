99d0cf20f546abe343f2dc26eb9e27c1aeb890c0
ashwin <ashwinkn@hotmail.com>
2019-03-17 10:21:36 -0500

    Fixes to run from global install

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
+import Path from 'path';
 // eslint-disable-next-line no-undef
 const requireFunc = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
-const basePath =
-    requireFunc.main.id === '.' ? requireFunc.main.id : requireFunc.resolve(`${requireFunc.main.id}/package.json`);
+const baseModule = requireFunc.main.children.find(module => module.id.includes('gen-sgen/dist/sgen.min.js'));
+const basePath = Path.dirname(Path.dirname(baseModule.id));
+
 const options = {
     // eslint-disable-next-line no-undef
     version: app.version,
