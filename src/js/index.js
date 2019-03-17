0883e405b27608cc8bd45161394041d37e1ffbff
ashwin <ashwinkn@hotmail.com>
2019-03-17 10:32:02 -0500

    Fixed path calcs

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,13 @@ import Path from 'path';
 // eslint-disable-next-line no-undef
 const requireFunc = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
 const baseModule = requireFunc.main.children.find(module => module.id.includes('gen-sgen/dist/sgen.min.js'));
-const basePath = Path.dirname(Path.dirname(baseModule.id));
+
+let basePath;
+if (!baseModule) {
+    basePath = '.';
+} else {
+    basePath = Path.dirname(Path.dirname(baseModule.id));
+}
 
 const options = {
     // eslint-disable-next-line no-undef
