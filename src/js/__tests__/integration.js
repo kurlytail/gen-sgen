e4a47038808e746582fb6ebfb25fd74f99146888
Ashwin Kamath <ashwinkn@hotmail.com>
2019-09-01 20:04:15 -0500

    Removed options test

--- src/js/__tests__/integration.js
+++ src/js/__tests__/integration.js
@@ -10,12 +10,6 @@ describe('# integration test', () => {
         execSync('git commit --allow-empty -m "Empty commit."', { cwd: 'testoutput' });
     });
 
-    it('## should print help options', () => {
-        let output = execSync('npm run build').toString();
-        output = execSync('sgen -g `pwd`/dist/sgen.min.js -h').toString();
-        expect(output).toMatchSnapshot();
-    });
-
     it('## should generate design and run sgen commands', () => {
         let output = execSync('npm run build').toString();
         output = execSync('sgen -g `pwd`/dist/sgen.min.js -d src/test/fixture/design.json -o testoutput').toString();
